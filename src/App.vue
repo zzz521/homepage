0c8b2914a0e36c7c8a4475f22f48b93b00855040
zhaobuyu <843792437@qq.com>
2025-04-01 21:12:04 +0800

    feat: 集成tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Hello COCO!" />
+  <h1 class="text-3xl font-bold text-blue-600">
+    Hello Tailwind CSS!
+  </h1>
 </template>
 
 <style scoped>

d4c48470492fc391a65b1ef7e4a1c7e961770bdf
zhaobuyu <843792437@qq.com>
2025-04-22 12:09:29 +0800

    feat: 修改类型问题

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/styles/main.scss';
-import 'virtual:svg-icons-register';
+import 'virtual:svg-icons-register'; //引入svg虚拟模块，自动注册所有svg图标
 
 
 createApp(App).mount('#app');
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+// src/svg-icons.d.ts
+declare module 'virtual:svg-icons-register' {
+    const component: any;
+    export default component;
+}
